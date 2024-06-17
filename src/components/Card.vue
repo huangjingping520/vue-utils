67c4b5c22f1b92f8b32c6b9032a870b0eb247620
MerlinAlex <merlinalex@qq.com>
2024-06-17 18:02:52 +0800

    feat: Update Card component to display name, description, and quantity

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
+import { toRefs } from 'vue'
+import type { ICardProps } from '@/types/components/card'
 
+const props = withDefaults(defineProps<ICardProps>(), {
+  name: 'Card Name',
+  description: 'Card Description',
+  quantity: 0
+})
+
+const { name, description, quantity } = toRefs(props)
 </script>
 
 <template>
-  <div>
-    card
+  <div border="1 solid blue-300" h32 w-xs rounded-2xl p4>
+    <div text-2xl font-black>
+      {{ name }}
+    </div>
+    <div h-16 flex gap-2>
+      <div line-clamp-3 w48 text-sm font-sans>
+        {{ description }}
+      </div>
+      <div w24 flex items-center justify-center text-2xl font-bold>
+        {{ quantity }}
+      </div>
+    </div>
   </div>
 </template>
---
+++ src/types/components/card.ts
@@ -0,0 +1,19 @@
+/**
+ * Represents the properties of a card component.
+ */
+export interface ICardProps {
+  /**
+   * The name of the card.
+   */
+  name: string
+
+  /**
+   * The description of the card.
+   */
+  description: string
+
+  /**
+   * The quantity of the card.
+   */
+  quantity: number
+}
